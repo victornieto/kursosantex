9f5b2b28893ed5c97c347a5522b853635815fe2a
efeser <erwin.feser@gmail.com>
2013-02-04 11:08:07 -0300

    form de ejemplo

--- kursoapp/admin.py
+++ kursoapp/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from models import HomePost, BlogPost, RespuestaBlogPost
+from models import HomePost, BlogPost, RespuestaBlogPost, Cliente, Diapositiva, Servicio
 
 admin.site.register(HomePost)
 admin.site.register(BlogPost)
 admin.site.register(RespuestaBlogPost)
-
+admin.site.register(Cliente)
+admin.site.register(Diapositiva)
+admin.site.register(Servicio)
 
 
